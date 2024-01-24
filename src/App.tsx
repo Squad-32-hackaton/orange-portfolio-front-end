3d33d0349674a9966676757b34a80ddf3203dd1b
Matthews Britto <84083804+MatthewsBritto@users.noreply.github.com>
2024-01-24 20:40:02 -0300

    init repo

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Button from '@mui/material/Button';
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Button variant="contained">Hello world</Button>;
+
+    </>
+  )
+}
+
+export default App
