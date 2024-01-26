034440223814a1a17748c62a4fd6d6338aae8b89
Matthews Britto <luismatthews29@gmail.com>
2024-01-26 09:28:23 -0300

    fix: align create project modal

--- src/components/ProjectModal/index.tsx
+++ src/components/ProjectModal/index.tsx
@@ -1,23 +1,39 @@
 import {  Typography } from "@mui/material";
-import { ModalContainer } from "./styles";
-import Input from '@mui/material/Input';
+import { ModalContainer, Container } from "./styles";
+import TextField from '@mui/material/TextField';
+
 
 
 export default function ProjectModal() {
   return (
-   <ModalContainer>
-    <Typography
-    sx={{color: '#515255', fontSize:'1.5rem'}}
-    > Adicionar projeto </Typography>
-    <Input 
-     placeholder="Titulo"
-    />
-    <Input 
-     placeholder="Tags"
-    />
-    <Input 
-     placeholder="Descrição"
-    />
-   </ModalContainer>
+    <Container>
+      <ModalContainer>
+
+        <Typography
+          sx={{ 
+           color: '#515255',
+           fontSize:'1.5rem' }}
+          > Adicionar projeto 
+        </Typography>
+        
+        <TextField 
+        placeholder="Titulo"
+        />
+        <TextField 
+        placeholder="Tags"
+        
+        />
+        <TextField 
+          placeholder="Descrição"
+          multiline
+          minRows={3}
+        />
+
+        <Typography variant="body1"  sx={{color: '#515255',letterSpacing:'0.15px',lineHeight: '16px'}}>
+          Selecione o conteúdo que você deseja fazer upload
+        </Typography>
+      </ModalContainer>   
+    </Container>
+   
   )
 }
--- src/components/ProjectModal/styles.ts
+++ src/components/ProjectModal/styles.ts
@@ -1,12 +1,22 @@
 import {styled} from '@mui/system'
 
+
+export const Container = styled('div')({
+ width:'100%',
+ height:'100%',
+ display:'flex',
+ alignItems:'center',
+ justifyContent:'center',
+})
+
 export const ModalContainer = styled('div')({
  width:'calc(100% - 3rem)',
  background:'#fff',
- padding:'1rem 0',
+ padding:'1rem 1.3rem 10.8rem 1.5rem',
  display:'flex',
  flexDirection:'column',
  gap:'1rem',
  minHeight:'16rem',
- margin:'auto'
+ margin:'0 1.5rem'
 })
+
