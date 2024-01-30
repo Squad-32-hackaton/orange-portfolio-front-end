084584243854e3213c0a91851ecdd77f4b44b91c
Juliana Maria de Sousa Mesquita <julianamariasousamesquita@id.uff.br>
2024-01-29 22:53:42 -0300

    feat: add integration authenticated

--- src/screens/Auth/index.tsx
+++ src/screens/Auth/index.tsx
@@ -1,30 +1,43 @@
 import React from 'react';
 import { Link as RouterLink } from 'react-router-dom';
-import { ContainerStyles, 
+import { 
+  ContainerStyles, 
   BoxStyles, 
   CardMediaStyles, 
   TypographyTitleStyles, 
   IconButtonStyles, 
-  GridStyles,
-  LinkStyles
-} from './styles'
-import {
-  Box,
-  Button,
-  Container,
-  Typography,
-  TextField,
-  Link,
-  CardMedia,
-  Grid,
-  CssBaseline,
-} from '@mui/material';
+  GridStyles, 
+  LinkStyles 
+} from './styles';
+import { 
+  Box, 
+  Button, 
+  Container, 
+  Typography, 
+  TextField, 
+  Link, 
+  CardMedia, 
+  Grid, 
+  CssBaseline } 
+from '@mui/material';
 import LoginImage from '../../assets/img/LoginImage.png';
-import LogoGoogle from '../../assets/img/logo googleg 48dp.png'
+import LogoGoogle from '../../assets/img/logo googleg 48dp.png';
 import IconButton from '../../components/IconButton';
+import authService from '../../services/authService';
 
 const LoginPage: React.FC = () => {
-  const handleLogin = () => {
+  const handleLogin = async (email: string, password: string) => {
+    try {
+      const response = await authService.login({ email, password });
+
+      if (response.status === 200) {
+        console.log('Login bem-sucedido');        
+      } else {
+        console.error(response.data.error);
+      }
+    } catch (error) {
+      console.error('Erro ao fazer login:', error);
+    }
   };
 
   return (
@@ -58,14 +71,14 @@ const LoginPage: React.FC = () => {
               </Typography>
 
               <IconButton 
-              title={'Entrar com Google'} 
-              icon={<img src={LogoGoogle} alt="Google Logo" />} 
-              onClick={() => {
-                // Lógica para entrar com o Google
-              }}
-              sx={IconButtonStyles}
-              aria-label='Botão Entrar com Google'
-            /> 
+                title={'Entrar com Google'} 
+                icon={<img src={LogoGoogle} alt="Google Logo" />} 
+                onClick={() => {
+                  // Lógica para entrar com o Google
+                }}
+                sx={IconButtonStyles}
+                aria-label='Botão Entrar com Google'
+              /> 
 
               <Grid
                 item
@@ -80,7 +93,9 @@ const LoginPage: React.FC = () => {
                 <form
                   onSubmit={(e) => {
                     e.preventDefault();
-                    handleLogin();
+                    const email = e.currentTarget.email.value;
+                    const password = e.currentTarget.password.value;
+                    handleLogin(email, password);
                   }}
                 >
                   <TextField
@@ -117,13 +132,13 @@ const LoginPage: React.FC = () => {
                 </form>
 
                 <Link
-                component={RouterLink}
-                to="/signup"
-                variant="body2"
-                sx={LinkStyles}
-              >
-                Cadastre-se
-              </Link>
+                  component={RouterLink}
+                  to="/signup"
+                  variant="body2"
+                  sx={LinkStyles}
+                >
+                  Cadastre-se
+                </Link>
 
               </Grid>
             </Grid>
