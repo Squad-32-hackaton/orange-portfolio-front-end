d013ad450fb920155997d2b7ba36c7f2f1c89d94
Matthews Britto <luismatthews29@gmail.com>
2024-01-30 12:51:58 -0300

    feature: install and configure react query

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -12,15 +12,47 @@ import ProfileImage from '../../assets/img/ProfileImage.png'
 import Button from '@mui/material/Button'
 import { Avatar, Box, Typography } from '@mui/material'
 
+import { useQuery } from '@tanstack/react-query'
+
+import axios from 'axios'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { useState } from 'react'
+
+interface UserProps {
+  user_id: number
+  avatar: string
+  email: string
+  first_name: string
+  last_name: string
+}
 interface ProfileProps {
   handleCreateANewProject: () => void
-  nameUser: string
 }
 
-export default function Profile({
-  handleCreateANewProject,
-  nameUser,
-}: ProfileProps) {
+export default function Profile({ handleCreateANewProject }: ProfileProps) {
+  const [user, setUser] = useState<UserProps>({} as UserProps)
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const query = useQuery({
+    queryKey: ['users'],
+    queryFn: async () => {
+      try {
+        const response = await axios.get(
+          'https://5a89-2804-868-d043-2252-2d74-8774-3013-58ff.ngrok-free.app/users',
+          {
+            headers: {
+              'ngrok-skip-browser-warning': 'true',
+            },
+          },
+        )
+        setUser(response.data[0])
+        return response.data[0]
+      } catch (error) {
+        throw new Error('erro ao fazer requisição' + error)
+      }
+    },
+  })
+
   return (
     <Box sx={profileContainer}>
       <Box sx={imageContainer}>
@@ -30,7 +62,7 @@ export default function Profile({
       <Box sx={textContainer}>
         <Box sx={profileText}>
           <Typography sx={userName} variant="h1">
-            {nameUser}
+            {`${user.first_name} ${user.last_name}`}
           </Typography>
           <Typography sx={country}>Brasil</Typography>
         </Box>
@@ -45,6 +77,7 @@ export default function Profile({
           ADICIONAR PROJETO
         </Button>
       </Box>
+      <ReactQueryDevtools />
     </Box>
   )
 }
