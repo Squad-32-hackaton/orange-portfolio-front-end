1fe1ba0391f917e1664117b400d1fd9549662fa3
Juliana Maria de Sousa Mesquita <julianamariasousamesquita@id.uff.br>
2024-01-30 22:17:55 -0300

    fix: remove files that are not used

--- src/components/auth/Login.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Login: React.FC = () => {
-  return <div>Login Component</div>;
-};
-
-export default Login;
--- src/components/auth/Signup.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Signup: React.FC = () => {
-  return <div>Signup Component</div>;
-};
-
-export default Signup;
