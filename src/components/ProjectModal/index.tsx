f4d684280ff9c4a135b365a1e7b84481491fc1a2
Matthews Britto <luismatthews29@gmail.com>
2024-01-31 21:23:42 -0300

    feature: implementing validation to projectModal component

--- src/components/ProjectModal/index.tsx
+++ src/components/ProjectModal/index.tsx
@@ -4,19 +4,48 @@ import {
   projectModalContainer,
   projectModalContent,
   image,
+  modalTitle,
+  imageContainerTitle,
+  submitButton,
+  cancelButton,
 } from './styles'
+// import { z } from 'zod'
 import TextField from '@mui/material/TextField'
 import UploaderImage from '../UploaderImage'
 import { useState } from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form'
+
+// const FormSchema = z.object({
+//   title: z.string(),
+//   tags: z.string().array().nonempty(),
+//   description: z.string().min(15),
+// })
+
+type FormSchemaProps = {
+  title: string
+  tags: string[]
+  description: string
+}
 
 type ProjectModalProps = {
   handleClose: () => void
 }
 
 export default function ProjectModal({ handleClose }: ProjectModalProps) {
+  // Local States
   const [selectedImage, setSelectedImage] = useState(null)
   const [previewImage, setPreviewImage] = useState('')
 
+  // React-Hook-Form Logic
+  const {
+    register,
+    handleSubmit,
+    // formState: { erros },
+  } = useForm<FormSchemaProps>()
+
+  const onSubmit: SubmitHandler<FormSchemaProps> = (data) => console.log(data)
+
+  // Uploader Image Logic
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   function handleGetImageFile(event: any) {
     const image = event.target.files[0]
@@ -28,78 +57,92 @@ export default function ProjectModal({ handleClose }: ProjectModalProps) {
   }
 
   return (
-    <Box sx={projectModalMain}>
-      <Box sx={projectModalContainer}>
-        <Box sx={projectModalContent}>
-          <Typography
-            sx={{
-              color: '#515255',
-              fontSize: '1.5rem',
-            }}
-          >
-            {' '}
-            Adicionar projeto
-          </Typography>
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      style={{
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '10px',
+      }}
+    >
+      <Box sx={projectModalMain}>
+        <Box sx={projectModalContainer}>
+          <Box sx={projectModalContent}>
+            <Typography sx={modalTitle}> Adicionar projeto</Typography>
+            <TextField
+              placeholder="Titulo"
+              {...register('title', { required: true })}
+            />
 
-          <TextField placeholder="Titulo" />
-          <TextField placeholder="Tags" />
-          <TextField placeholder="Descrição" multiline minRows={3} />
+            <TextField
+              placeholder="Tags"
+              {...register('tags', { required: true })}
+            />
 
-          <Typography
-            variant="body1"
-            sx={{
-              color: '#515255',
-              letterSpacing: '0.15px',
-              lineHeight: '16px',
-            }}
-          >
-            Selecione o conteúdo que você deseja fazer upload
-          </Typography>
+            <TextField
+              placeholder="Descrição"
+              {...register('description', { required: true })}
+              multiline
+              minRows={2}
+            />
 
-          {selectedImage ? (
-            <img src={previewImage} alt="" style={image} />
-          ) : (
-            <label htmlFor="imageUploader">
-              <UploaderImage
-                texts={[
-                  {
-                    content: 'Compartilhe seu talento com milhares de pessoas',
-                    type: 'subTitle',
-                  },
-                ]}
-              />
-            </label>
-          )}
-          <Input
-            type="file"
-            id="imageUploader"
-            sx={{ display: 'none' }}
-            onChange={handleGetImageFile}
-          />
+            <Typography variant="body1" sx={imageContainerTitle}>
+              Selecione o conteúdo que você deseja fazer upload
+            </Typography>
 
-          <Typography variant="body1" color="#00000091">
-            Visualizar publicação
-          </Typography>
+            {selectedImage ? (
+              <img src={previewImage} alt="" style={image} />
+            ) : (
+              <label htmlFor="imageUploader">
+                <UploaderImage
+                  texts={[
+                    {
+                      content:
+                        'Compartilhe seu talento com milhares de pessoas',
+                      type: 'subTitle',
+                    },
+                  ]}
+                />
+              </label>
+            )}
+            <Input
+              type="file"
+              id="imageUploader"
+              sx={{ display: 'none' }}
+              onChange={handleGetImageFile}
+            />
 
-          <Stack
-            direction="row"
-            spacing={2}
-            sx={{ justifyContent: 'space-between' }}
-          >
-            <Button variant="contained" sx={{ width: '45%' }} size="large">
-              Salvar
-            </Button>
-            <Button
-              variant="contained"
-              sx={{ width: '45%', backgroundColor: '#00000061' }}
-              size="large"
-              onClick={() => handleClose()}
+            <Typography variant="body1" color="#00000091">
+              Visualizar publicação
+            </Typography>
+
+            <Stack
+              direction="row"
+              spacing={2}
+              sx={{ justifyContent: 'space-between' }}
             >
-              Cancelar
-            </Button>
-          </Stack>
+              <Button
+                variant="contained"
+                sx={submitButton}
+                size="large"
+                type="submit"
+              >
+                Salvar
+              </Button>
+              <Button
+                variant="contained"
+                sx={cancelButton}
+                size="large"
+                onClick={() => handleClose()}
+              >
+                Cancelar
+              </Button>
+            </Stack>
+          </Box>
         </Box>
       </Box>
-    </Box>
+    </form>
   )
 }
--- src/components/ProjectModal/styles.ts
+++ src/components/ProjectModal/styles.ts
@@ -27,3 +27,22 @@ export const image = {
   width: '100%',
   height: '100%',
 }
+
+export const modalTitle = {
+  color: '#515255',
+  fontSize: '1.5rem',
+}
+
+export const imageContainerTitle = {
+  color: '#515255',
+  letterSpacing: '0.15px',
+  lineHeight: '16px',
+}
+export const submitButton = {
+  width: '45%',
+}
+
+export const cancelButton = {
+  width: '45%',
+  backgroundColor: '#00000061',
+}
