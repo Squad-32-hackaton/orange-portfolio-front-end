213463da1ae6d20d3ee31283c90c8a3eb00d38b5
danielmouraoliveira1995 <danielmouraoliveira1995@gmail.com>
2024-02-05 20:15:26 -0300

    correção do botao de edição e responsividade do grid

--- src/components/CardProject/index.tsx
+++ src/components/CardProject/index.tsx
@@ -1,124 +1,48 @@
 import {
-  container,
-  divInfos,
-  divForaInfos,
-  divTags,
-  tagStyle,
   aling,
+  container,
+  divForaInfos,
+  divInfos,
+  divTags,
   imageContainer,
+  tagStyle,
   userName,
-  iconContainer,
-  penIcon,
-  menuListContainer,
 } from './styles'
 
-import ProfilePhoto from '../../assets/img/ProfileImage.png'
+import Card from '../../assets/img/CardProjectLarge.png'
 
-import { Avatar, Box, Button, Menu, MenuItem, Typography } from '@mui/material'
-import { ProjectsContext } from '../../contexts/ProjectsContext'
-import React, { useContext } from 'react'
-import { ModalContext } from '../../contexts/ModalsContext'
-import { Edit } from '@mui/icons-material'
+import { Avatar, Box, Typography } from '@mui/material'
+import ProfilePhoto from '../../assets/img/ProfileImage.png'
+import { CardProjectDrawer } from '../CardProjectDrawer'
 
 interface CardDataProps {
   name: string
   avatar: string
   tags: string[]
   projectId: number
-  image: string
-  creationAt: string
 }
 
-export default function CardProject({
-  name,
-  tags,
-  projectId,
-  image,
-  creationAt,
-}: CardDataProps) {
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
-  const open = Boolean(anchorEl)
-  const {
-    handleOpenDeleteModal,
-    handleSetCurrentModalType,
-    handleModalCreateANewProject,
-  } = useContext(ModalContext)
-  const { handleSetCurrentProject } = useContext(ProjectsContext)
-
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget)
-  }
-  const handleClose = () => {
-    setAnchorEl(null)
-  }
-
-  const [isHovered, setIsHovered] = React.useState(false)
-
-  function handleEditProject() {
-    handleSetCurrentModalType('edit')
-    handleModalCreateANewProject()
-  }
+export default function CardProject({ name, tags, projectId }: CardDataProps) {
 
   return (
-    <Box
-      sx={container}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-    >
+    <Box sx={container}>
       <Box sx={aling}>
         <Box sx={imageContainer}>
-          <img
-            src={`https://orangeportfoliosquad32.software/images/${image}`}
-            alt=""
-          />
-
-          {isHovered && (
-            <Box
-              sx={iconContainer}
-              onClick={() => handleSetCurrentProject(projectId)}
-            >
-              <Button
-                id="basic-button"
-                aria-controls={open ? 'basic-menu' : undefined}
-                aria-haspopup="true"
-                aria-expanded={open ? 'true' : undefined}
-                onClick={handleClick}
-                style={{ minWidth: '0' }}
-              >
-                <Edit sx={penIcon} />
-              </Button>
-              <Menu
-                id="basic-menu"
-                anchorEl={anchorEl}
-                open={open}
-                onClose={handleClose}
-                MenuListProps={{
-                  'aria-labelledby': 'basic-button',
-                }}
-                sx={menuListContainer}
-              >
-                <MenuItem onClick={handleEditProject}>Editar</MenuItem>
-                <MenuItem onClick={handleOpenDeleteModal}>Excluir</MenuItem>
-              </Menu>
-            </Box>
-          )}
+          <img src={Card} alt="" />
+          <CardProjectDrawer projectId={projectId} />
         </Box>
         <Box sx={divForaInfos}>
           <Box sx={divInfos}>
             <Avatar src={ProfilePhoto} />
-            <Typography sx={userName}>
-              {`${name}`} . {creationAt}{' '}
-            </Typography>
+            <Typography sx={userName}>{`${name}`} . 02/24</Typography>
           </Box>
           <Box sx={divTags}>
             {tags &&
-              tags.map((tag) => {
-                return (
-                  <Box sx={tagStyle} key={tag}>
-                    {tag}
-                  </Box>
-                )
-              })}
+              tags.map((tag) => (
+                <Box sx={tagStyle} key={tag}>
+                  {`${tag.slice(0, 10)}${tag.length > 10 ? '...' : ''}`}
+                </Box>
+              ))}
           </Box>
         </Box>
       </Box>
