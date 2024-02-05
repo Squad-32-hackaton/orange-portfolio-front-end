d9683d93e5fe3b63ec89cda59c19e1ac18642e77
danielmouraoliveira1995 <danielmouraoliveira1995@gmail.com>
2024-02-05 20:42:08 -0300

    fix

--- src/components/CardProject/styles.ts
+++ src/components/CardProject/styles.ts
@@ -137,5 +137,26 @@ export const userName = {
 
 }
 
+export const penIcon = {
+  padding: '2px',
+  background: '#FFCC99',
+  borderRadius: '50%',
+  color: '#000000',
+  border: 'none',
+}
+
+export const iconContainer = {
+  position: 'relative',
+  right: '60px',
+  top: '10px',
+
+}
+
+export const menuListContainer = {
+  ul: {
+    width: '208px',
+  },
+}
+
 
 
